4b86d0dae1d2807abef64b6591e1fdb2a3a89e16
knmr216 <nomunomu1139@gmail.com>
2021-02-03 20:04:45 +0900

    バリデーションの設定とビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  with_options presence: true do
+    validates :nickname
+    validates :name, format: { with: /\A[a-zA-Z0-9_]{4,}\z/ }
+  end
 end
