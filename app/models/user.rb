8e33a2a27e1ad785f4ff95aac2747840d7c8ce10
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-05 11:59:00 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :email, uniqueness: true
   with_options presence: true do
     validates :nickname
-    validates :name, format: { with: /\A[a-zA-Z0-9_]{4,}\z/ }
+    validates :name, format: { with: /\A[a-z\d]{1}[a-z\d_]{3,}\z/i }
   end
 end
