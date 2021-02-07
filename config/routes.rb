00f2cdf10a1cbae900617c54d544bf0d277fc81b
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-07 18:11:59 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :brands
-  devise_for :users
+  devise_for :brands, controllers: {
+    sessions: 'brands/sessions',
+    passwords: 'brands/passwords',
+    registrations: 'brands/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
   root to: 'items#index'
   resources :items, only: :index
 end
