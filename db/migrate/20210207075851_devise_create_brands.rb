c00e395603e08d19fdb78dc68127f4d1754b0995
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-07 17:45:48 +0900

    brandsテーブルの作成

--- db/migrate/20210207075851_devise_create_brands.rb
+++ db/migrate/20210207075851_devise_create_brands.rb
@@ -4,6 +4,8 @@ class DeviseCreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
       ## Database authenticatable
+      t.string :name,               null: false
+      t.text   :introduction
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
