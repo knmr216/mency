25097ed43352625a58baead9d32f8fa26f27f0c6
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-10 18:49:18 +0900

    unitモデルのデータの定義

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,4 +1,10 @@
 class Unit < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'ml' },
+    { id: 3, name: 'g' }
+  ]
+  
   include ActiveHash::Associations
   has_many :items
 end
