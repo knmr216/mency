6c4b3569e76cdb26f704999954c4f84c9208b226
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-10 22:04:44 +0900

    newアクションとcreateアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category_id, :capacity, :unit_id, :price, :introduction).merge(brand_id: current_brand.id)
+  end
+
 end
