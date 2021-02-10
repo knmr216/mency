de1e4355266707c872a89e8310ae9e80c4f57d61
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-10 19:35:28 +0900

    バリデーションとアソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,18 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :unit
   belongs_to :brand
+
+  with_options presence: true do
+    validates :name
+    validates :capacity
+    validates :price
+    validates :introduction
+  end
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :unit_id
+  end
+
 end
