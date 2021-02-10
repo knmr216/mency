f36c97e73c957e1fc736011931c0db3d0a40747f
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-10 19:08:17 +0900

    ActiveHashデータの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,13 @@
 class Category < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '化粧水' },
+    { id: 2, name: '美容液' },
+    { id: 3, name: '乳液' },
+    { id: 4, name: 'クレンジング' },
+    { id: 5, name: '洗顔料' }
+  ]
+
   include ActiveHash::Associations
   has_many :items
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,10 +1,11 @@
 class Unit < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: 'ml' },
-    { id: 3, name: 'g' }
+    { id: 0, name: '--' },
+    { id: 1, name: 'ml' },
+    { id: 2, name: 'g' },
+    { id: 3, name: '個' }
   ]
-  
+
   include ActiveHash::Associations
   has_many :items
 end
