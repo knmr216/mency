22192da269a817298123f42ce59a0629af0f998b
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-15 11:00:24 +0900

    バリデーションの設定とコントローラーの修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :move_to_index, only: :new
 
   def new
     @item = Item.find(params[:item_id])
@@ -8,16 +9,21 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     if @review.save
-      redirect_to item_path()
+      redirect_to item_path(params[:item_id])
     else
+      @item = Item.find(params[:item_id])
       render :new
     end
   end
 
   private
 
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
+
   def review_params
-    params.require(:review).permit(:score, :text, :image).merge(user_id: current_user.id)
+    params.require(:review).permit(:score, :text, :image).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
