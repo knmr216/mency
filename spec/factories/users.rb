2144ea960248008513976fb866f871c706c38c1e
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-22 09:39:21 +0900

    userモデルのインスタンスの作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { Faker::Name.name }
+    email ( Faker::internet.email )
+    password { Faker::internet.password }
+    password_confirmation { password }
+    introduction { Faker::Lorem.sentence }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png')) }
   end
 end
