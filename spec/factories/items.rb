462f93a59544ddcc4ea1121ca6d6491ef70bd097
knmr216 <kohei.nomura.030216@gmail.com>
2021-02-26 17:30:37 +0900

    itemモデルのFactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Name.name }
+    category_id { Faker::Number.between(from: 1, to: 5) }
+    capacity { Faker::Number.number }
+    unit_id { Faker::Number.between(from: 1, to: 3) }
+    price {Faker::Number.number }
+    introduction { Faker::Lorem.sentence }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png')) }
+    association :brand
   end
 end
