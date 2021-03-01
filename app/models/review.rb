69a773aefc3dcf2da88d49a355af8949ea042007
knmr216 <kohei.nomura.030216@gmail.com>
2021-03-01 19:02:52 +0900

    reviewモデルの編集
    Uploaderの設定の書き忘れ

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review < ApplicationRecord
   belongs_to :item
   belongs_to :user
 
+  mount_uploader :image, ImageUploader
+
   with_options presence: true do
     validates :score
     validates :text
